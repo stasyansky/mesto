171a7c1139cab51e8eb5f2a2e9cbf6b63819d070
Stanislav Ivanisov <st.ivanisov@yandex.ru>
2022-02-27 22:21:39 +0300

    feat: доработал файл script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,33 @@
+const editProfile = document.querySelector('.profile__edit-btn');
+const profileName = document.querySelector('.profile__name');
+const profileProf = document.querySelector('.profile__prof');
 const popup = document.querySelector('.popup');
-const editProfile = document.querySelector('.profile__edit-btn')
-const openPopup = document.querySelector('.popup_opened');
-const closePopup = document.querySelector('.popup__close-btn');
+const closePopup = popup.querySelector('.popup__close-btn');
+let formEditProfile = popup.querySelector('.popup__form');
+let inputName = popup.querySelector('.popup__item_type-name');
+let inputProf = popup.querySelector('.popup__item_type-prof');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
 }
-editProfile.addEventListener('click', function (event) {
-    togglePopup();
-})
-closePopup.addEventListener('click', function (event) {
-     togglePopup();
- })
 
+editProfile.addEventListener('click', function () {
+    togglePopup();
+    inputName.value = profileName.textContent;
+    inputProf.value = profileProf.textContent;
+});
+
+closePopup.addEventListener('click', togglePopup);
 popup.addEventListener('click', function (event) {
   if (event.target === event.currentTarget) {
     togglePopup();
   }
-})
+});
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileProf.textContent = inputProf.value;
+  togglePopup();
+}
+formEditProfile.addEventListener('submit', formSubmitHandler);
