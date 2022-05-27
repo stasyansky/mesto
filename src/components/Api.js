528ccc3e291c0f50603b841743470fb07cfabc3b
Stanislav Ivanisov <st.ivanisov@yandex.ru>
2022-05-27 17:54:01 +0300

    Добавил новые файлы

---
+++ src/components/Api.js
@@ -0,0 +1,65 @@
+export class Api {
+  constructor(config) {
+    this._url = config.url;
+    this._headers = config.headers;
+  }
+
+  _errorHandler = (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: this._headers
+    }).then(this._errorHandler);
+  }
+
+  editUserInfo(data) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    }).then(this._errorHandler);
+  }
+
+  getInitialCards() {
+    return fetch(`${this._url}/cards`, {
+      method: 'GET',
+      headers: this._headers
+    }).then(this._errorHandler);
+  }
+
+  postNewCard(data) {
+    return fetch(`${this._url}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    }).then(this._errorHandler);
+  }
+
+  toggleLike({ cardId, isLikedByMe }) {
+    return fetch(`${this._url}/cards/${cardId}/likes`, {
+      method: isLikedByMe ? 'DELETE' : 'PUT',
+      headers: this._headers,
+    }).then(this._errorHandler);
+  }
+
+  cardDelete(cardId) {
+    return fetch(`${this._url}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    }).then(this._errorHandler);
+  }
+
+  avatarUpdate(avatar) {
+    return fetch(`${this._url}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({ avatar })
+    }).then(this._errorHandler);
+  }
+}
